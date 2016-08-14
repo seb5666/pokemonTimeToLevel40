b925b05ed83664171d1995f62c98e4ed0f4f12c6
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2016-08-14 23:29:18 +0100

    Add initial server and gitignore file.

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    logger = require("morgan"),
+    app = express();
+
+app.use(logger("dev"));
+app.use(express.static(__dirname + "/static"));
+
+
+app.get("/", function(req, res, next) {
+    try {
+        res.send("<h1>Hello World");
+    } catch(e) {
+        next(e);
+    }
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Listening on http://localhost:" + (process.env.PORT || 3000));
+});
