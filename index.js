0384122a7a5617af18d07ff375f89c8a90fe95b8
Sebastian Borgeaud <sebastian.borgeaud@gmail.com>
2016-08-17 21:38:15 +0100

    Add error handling on form inputs.

--- index.js
+++ index.js
@@ -21,23 +21,47 @@ app.get("/", function(req, res, next) {
 });
 
 app.post("/result", function(req, res, next) {
-    //TODO error handling
     console.log(req.body);
     try {
         var level = +req.body.level;
         var currentXP = +req.body.currentXP;
-        var xpGained= currentXP + XP[level]
         var startDate = new Date(req.body.startDate);
+       
+        if (level === 40) {
+            res.render("error", {
+                errorMessage: "Congratulations, you are already level 40!" 
+            });
+            return;
+        }
+        
         var now = new Date();
+
+        if (level < 1 || level > 39 || currentXP < 0 || currentXP > XP[level] || startDate.getFullYear() < 2016 || startDate > now) {
+            res.render("error", {
+                errorMessage: "Some inputs were invalid on the form. Verify that the level is between 1 and 40 and that the current XP is correct." 
+            });
+            return;
+        }
+
+        var xpGained= currentXP + XP[level]
         var xpPerMilliSecond = xpGained / (now - startDate);
         var milliSecondsTill40 = (XP[40] - xpGained) / xpPerMilliSecond;
         var finalDate = new Date(now.getTime() + milliSecondsTill40);
+        
+        if (isNaN(finalDate.getTime())) {
+            res.render("error", {
+                errorMessage: "Sorry, but you will be long dead by the time you reach level 40."
+            });
+            return;
+        }
+        
         var milliSecondsPerDay = 1000 * 60 * 60 * 24;
         var milliSecondsPerYear = milliSecondsPerDay * 365;
 
         var missingYears = Math.floor(milliSecondsTill40 / milliSecondsPerYear);
-        var missingDays = Math.floor((milliSecondsTill40 % (missingYears * milliSecondsPerYear)) / milliSecondsPerDay);
-        
+        var missingDays = (missingYears > 0) ? Math.floor((milliSecondsTill40 % (missingYears * milliSecondsPerYear)) / milliSecondsPerDay) :  Math.floor(milliSecondsTill40 / milliSecondsPerDay);
+        console.log(missingYears);
+       console.log(missingDays); 
         res.render("result", {
             finalDate: prettyDate.print(finalDate),
             xpGained: xpGained,
@@ -49,7 +73,9 @@ app.post("/result", function(req, res, next) {
         });
     } catch (e) {
         console.log(e);
-        res.send("An error occured while calculating the time needed for you to reach level 40.");
+        res.render("error", {
+            errorMessage: "An error occured while calculating the time needed for you to reach level 40."
+        });
     }
 });
 
